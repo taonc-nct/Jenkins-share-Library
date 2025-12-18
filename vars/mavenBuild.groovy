ba01ec595ce951b9cdb7f1b9904436e0e2409a80
taonc#nct <89116160+taonc-nct@users.noreply.github.com>
2025-12-18 18:04:00 +0700

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,13 +1,8 @@
 def build() {
-    sh 'pwd'
-    sh 'ls -la'
-    // Build Maven
-    def mvnCommand = 'mvn clean package -Dmaven.test.skip=true'
+    def mvnCommand = 'mvn -B -Dmaven.repo.local=/root/.m2/repository clean install -DskipTests'
     def exitCode = sh(script: mvnCommand, returnStatus: true)
 
     if (exitCode != 0) {
-        error "Maven build failed with exit code: ${exitCode}"
+        error "Maven build failed with exit code: $exitCode"
     }
 }
-
-return this
