216a596604e35d26c713ee475a2c9bf3b88feeb9
taonc#nct <89116160+taonc-nct@users.noreply.github.com>
2025-12-18 18:00:29 +0700

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,17 +1,18 @@
 def build() {
+    // Checkout Git repo từ pipeline SCM
+    checkout scm
+
+    // Debug xem đã có code chưa
+    sh 'pwd'
+    sh 'ls -la'
+
+    // Build Maven
     def mvnCommand = 'mvn clean package -Dmaven.test.skip=true'
     def exitCode = sh(script: mvnCommand, returnStatus: true)
 
     if (exitCode != 0) {
-        error "Maven build failed with exit code: $exitCode"
+        error "Maven build failed with exit code: ${exitCode}"
     }
 }
 
-// def test() {
-//     def mvnCommand = 'mvn test'
-//     def exitCode = sh(script: mvnCommand, returnStatus: true)
-
-//     if (exitCode != 0) {
-//         error "Maven test failed with exit code: $exitCode"
-//     }
-// }
+return this
