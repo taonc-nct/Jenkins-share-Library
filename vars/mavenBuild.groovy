355bb7bd3487b11c6a9ca68df55bacabd592ccd6
taonc#nct <89116160+taonc-nct@users.noreply.github.com>
2025-12-19 16:40:25 +0700

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,18 +1,4 @@
 def build() {
-
-    def mvncheck = '''
-        echo "=== DEBUG WORKSPACE ==="
-        pwd
-        ls -la
-        echo "=== FIND pom.xml ==="
-        find . -name pom.xml
-    '''
-    def exitCodecheck = sh(script: mvncheck, returnStatus: true)
-
-    if (exitCodecheck != 0) {
-        error "Không tìm thấy pom.xml hoặc workspace có vấn đề"
-    }
-
     def mvnCommand = '''
         mvn -B \
         -Dmaven.repo.local=$WORKSPACE/.m2/repository \
